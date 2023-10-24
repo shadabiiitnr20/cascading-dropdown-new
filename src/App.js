2a6afb9f7ef8304b1401fb7c827ade7145cca280
Md. Shadab Hussain <shadab.iiitnr20@gmail.com>
2023-10-24 13:57:09 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import { countryList, stateList, cityList } from "./constants/data";
 
 function App() {
+  const [country, setCountry] = useState([]);
+  const [state, setState] = useState([]);
+  const [city, setCity] = useState([]);
+
+  useEffect(() => {
+    setCountry(countryList);
+  }, []);
+
+  const handleState = (id) => {
+    const filteredState = stateList.filter((state) => state.countryId === id);
+    setState(filteredState);
+  };
+
+  const handleCity = (id) => {
+    const filteredCity = cityList.filter((city) => city.stateId === id);
+    setCity(filteredCity);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <h2 className="text-xl underline font-semibold m-4">
+        Cascading dropdown
+      </h2>
+      <>
+        <h2 className="text-sm font-bold underline m-2">Select Country</h2>
+        <select
+          className="border border-black border-solid p-2 m-2 rounded-lg shadow-md"
+          onChange={(e) => handleState(e.target.value)}
         >
-          Learn React
-        </a>
-      </header>
+          <option value="0">Select Country</option>
+          {country.map((country) => {
+            return (
+              <option key={country.id} value={country.id}>
+                {country.name}
+              </option>
+            );
+          })}
+        </select>
+      </>
+      <>
+        <h2 className="text-sm font-bold underline m-2">Select State</h2>
+        <select
+          className="border border-black border-solid p-2 m-2 rounded-lg shadow-md"
+          onChange={(e) => handleCity(e.target.value)}
+        >
+          <option value="0">Select State</option>
+          {state.map((state) => {
+            return (
+              <option key={state.id} value={state.id}>
+                {state.name}
+              </option>
+            );
+          })}
+        </select>
+      </>
+      <>
+        <h2 className="text-sm font-bold underline m-2">Select City</h2>
+        <select className="border border-black border-solid p-2 m-2 rounded-lg shadow-md">
+          <option value="0">Select City</option>
+          {city.map((city) => {
+            return (
+              <option key={city.id} value={city.id}>
+                {city.name}
+              </option>
+            );
+          })}
+        </select>
+      </>
     </div>
   );
 }
---
+++ src/constants/data.js
@@ -0,0 +1,76 @@
+export const countryList = [
+  {
+    id: "1",
+    name: "India",
+  },
+  {
+    id: "2",
+    name: "USA",
+  },
+];
+
+export const stateList = [
+  {
+    id: "1",
+    countryId: "1",
+    name: "MH",
+  },
+  {
+    id: "2",
+    countryId: "1",
+    name: "CG",
+  },
+  {
+    id: "3",
+    countryId: "2",
+    name: "NY",
+  },
+  {
+    id: "4",
+    countryId: "2",
+    name: "NV",
+  },
+];
+
+export const cityList = [
+  {
+    id: "1",
+    stateId: "1",
+    name: "Mumbai",
+  },
+  {
+    id: "2",
+    stateId: "1",
+    name: "Pune",
+  },
+  {
+    id: "3",
+    stateId: "2",
+    name: "Bilaspur",
+  },
+  {
+    id: "4",
+    stateId: "2",
+    name: "Raipur",
+  },
+  {
+    id: "5",
+    stateId: "3",
+    name: "NY 1",
+  },
+  {
+    id: "6",
+    stateId: "3",
+    name: "NY 2",
+  },
+  {
+    id: "7",
+    stateId: "4",
+    name: "NV 1",
+  },
+  {
+    id: "8",
+    stateId: "4",
+    name: "NV 2",
+  },
+];
